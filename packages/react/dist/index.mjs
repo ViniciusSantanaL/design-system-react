a7694bf9568656469d7e6e254e1da28f0cb88bb4
Vinicius Leao <vinicius.s.silva-kee@parceirosedu.com.br>
2023-09-11 18:14:49 -0300

    first commit

---
+++ packages/react/dist/index.mjs
@@ -0,0 +1,8 @@
+// src/index.tsx
+import { jsx } from "react/jsx-runtime";
+function App() {
+  return /* @__PURE__ */ jsx("div", { children: /* @__PURE__ */ jsx("h1", { children: "Hello World!" }) });
+}
+export {
+  App
+};
